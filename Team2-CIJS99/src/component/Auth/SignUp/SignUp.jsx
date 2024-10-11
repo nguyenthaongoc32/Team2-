dcb766ef2d1e387d54e9c8e50a3362ade6e10c55
Nguyen Thao Ngoc <ngocnguyenthao140@gmail.com>
2024-10-11 17:28:37 +0700

    first signUp

---
+++ Team2-CIJS99/src/component/Auth/SignUp/SignUp.jsx
@@ -0,0 +1,119 @@
+import React, { useState } from 'react'
+import '../SignUp/SignUp.css'
+import { useNavigate } from 'react-router-dom';
+import { auth, googleProvider, createUserWithEmailAndPassword,signInWithPopup,signOut } from '../firebaseConfig';
+function SignUp(){
+    const [email, setEmail] = useState('');
+    const [password , setPassword] = useState('');
+    const[confirmPassword , setConfirmPassword] = useState('');
+    const[error, setError] = useState('');
+    const [success, setSuccess]=useState('');
+    const navigate = useNavigate();
+
+    const validateEmail = (email) => {
+        const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+        return emailRegex.test(email);
+    };
+    const validatePassword=(password) =>{
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{6,}$/;
+        return passwordRegex.test(password);
+      };
+    
+    const handleSignUp = (e) =>{ 
+        e.preventDefault();
+        setError('');
+        setSuccess('');
+        if (password !== confirmPassword){
+            setError('Password and confirm password do not match');
+            return;
+        };
+        if (!validateEmail(email)){
+            setError('Invalid email');
+        };
+        if(!validatePassword(password)){
+            setError('Password must be at least 6 characters including at least 1 uppercase letter, 1 lowercase letter, 1 number and 1 character');
+            return;
+        }
+        createUserWithEmailAndPassword(auth,email,password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            setSuccess('Sign up successful!');
+            navigate('/',{state:{userEmail:user.email}});
+        }).catch((error) =>{
+            setError(error.message);
+        })
+    };
+    const handleSignUpGoogle = () =>{
+        signInWithPopup(auth,googleProvider)
+        .then((result) =>{
+            setSuccess('Sign up with Google successful!');
+            const userLocal = {
+                email : response.user.email,
+            };
+            localStorage.setItem("userLocal", JSON.stringify(userLocal));
+            const user = result.user;
+        navigate('/', {state:{userEmail:user.email}});
+            })
+            .catch((error) =>{
+                setError(error.message);
+            });
+    };
+    
+    return (
+        <div className='signup-page'>
+            <div className='background-page'></div>
+            <main className='main-sigup'>
+                <div className='container-signup'>
+                    <form className='signup-form' onSubmit={handleSignUp}>
+                        <h1 className='title-signup'>SignUp</h1>
+                        <div className='signup-inputbox'>
+                            <input type="text" placeholder='' id='user-signup'
+                             className='signup-field' 
+                             required
+                             onChange={(e) => setEmail(e.target.value)} />
+                            <label className='signup-label'> Email</label>
+                        </div>
+                        <div className='signup-inputbox'>
+                            <input type="text" placeholder='' id='user-signup'
+                             className='signup-field' 
+                             required />
+                            <label className='signup-label'>User Name</label>
+                        </div>
+                        <div className='signup-inputbox'>
+                            <input type="text" placeholder='' className='signup-field'
+                            onChange={(e) => setPassword(e.target.value)} required />
+                            <label className='signup-label'>Password</label>
+                        </div>
+                        <div className='signup-inputbox'>
+                            <input type="text" placeholder='' className='signup-field'
+                            onChange={(e) => setConfirmPassword(e.target.value)} required />
+                            <label className='signup-label'>Confirm Password</label>
+                        </div>
+                        {error && <p style={{color:"red"}}>{error}</p>}
+                        {success && <p style={{color:"green"}}>{success}</p>}
+                        <button className='btn-signup' type='submit'>SignUp</button>
+                        
+                        <div className='choose'>
+                            <span className='span-choose-signup'>Hoặc</span>
+                        </div>
+                        <div className='social-btn'>
+                                <button className='btn-gg' onClick={handleSignUpGoogle}>
+                                    <img  src='/img/gmail.jpg' className="gmail" alt="logo" />
+                                    Sign up with Google
+                                </button>
+                            </div>
+                            <div className='login'>
+                            <p className='login-p'>
+                                Have an account?
+                                {/* <Link className='login-link' to='/login'>LogIn</Link> */}
+                            </p>
+                        </div>
+                    </form>
+                </div>
+
+            </main>
+        </div>
+    )
+}
+
+export default SignUp
